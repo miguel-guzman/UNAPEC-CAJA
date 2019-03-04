45de6587e732acce0f434da66a7acbdd40adde69
bcsolutions2 <briancorrea0311@gmail.com>
2019-03-04 15:15:35 -0400

    buscador empleado

--- Django/UNAPEC/settings.py
+++ Django/UNAPEC/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xe_b-!&#+px%2acwo+cyq!j)o63o&uz8!lsg0-^@=zdr!i-*5d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.10', 'localhost']
 
 
 # Application definition
